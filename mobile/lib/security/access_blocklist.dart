5b49bd7d093f062ce2fad2d01851708569d39309
Aaron Paszek <aaronpaszek@icloud.com>
2026-08-20 22:05:26 +1000

    fix: correct blocked phone hash

--- mobile/lib/security/access_blocklist.dart
+++ mobile/lib/security/access_blocklist.dart
@@ -15,7 +15,7 @@ class AccessBlocklist {
 
   static const Set<String> _blockedPhoneHashes = {
     '9605401255b50d556b4bcc07c442b65ac9cbd71691bd167ff18cc8249a3f251b',
-    '4fe958cb52ca69de5de2cb3f532ecd7dff2c61c0e58bbc7070bd5853f7efb65',
+    '4fe958cb52ca69de5de2cb3f532ecd7dff2c61c0e58bbc7070bd585f3f7efb65',
   };
 
   static String normalizePhone(String phone) {
