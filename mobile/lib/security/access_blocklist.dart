36bd11f49fdfe8a312344b4491d95faf43e8b8f9
Aaron Paszek <aaronpaszek@icloud.com>
2026-08-26 13:07:34 +1000

    security: add three blocked phone identifiers to access blocklist

--- mobile/lib/security/access_blocklist.dart
+++ mobile/lib/security/access_blocklist.dart
@@ -16,6 +16,9 @@ class AccessBlocklist {
   static const Set<String> _blockedPhoneHashes = {
     '9605401255b50d556b4bcc07c442b65ac9cbd71691bd167ff18cc8249a3f251b',
     '4fe958cb52ca69de5de2cb3f532ecd7dff2c61c0e58bbc7070bd585f3f7efb65',
+    '9155a4747c0f402956eb6741ce1a832a945686b364a1e3bbc56beaafaac43309',
+    'de73f46d41e9a225f767c310b342a596ec8640dfdaa98c42b3ad23c9c259247e',
+    '6a3e13fe4ca63e99c4b586274a32e66251856a69c08e46aa566f1c90df96024a',
   };
 
   static String normalizePhone(String phone) {
